42545c0c571c4e5252cd2ba384e26dafa226a997
zhuozhou <1174530834@qq.com>
2021-08-23 23:50:05 +0800

    master

--- src/main/java/com/atguigu/test/gitTest.java
+++ src/main/java/com/atguigu/test/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("git hello!");
         System.out.println("git hello! san");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2");
     }
 }
